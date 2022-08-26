a62808b91545b6dd1ab05816027e8a535386eed0
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2022-08-26 15:46:45 -0400

    Initial Commit

---
+++ Gamescg/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gamescg
+//
+//  Created by Ayaan Shahab on 2022-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gamescg/GamescgApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamescgApp.swift
+//  Gamescg
+//
+//  Created by Ayaan Shahab on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct GamescgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
