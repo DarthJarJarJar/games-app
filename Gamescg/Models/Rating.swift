44992809a6fdfbefa98b8d8985db692dfa878330
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2022-09-03 16:24:57 -0400

    empty

---
+++ Gamescg/Models/Rating.swift
@@ -0,0 +1,13 @@
+//
+//  Rating.swift
+//  Gamescg
+//
+//  Created by Ayaan  on 2022-08-31.
+//
+
+import Foundation
+
+struct Rating: Identifiable, Decodable {
+    var id: Int
+    var rating: Int
+}
